12c6e945cd1fff137fbc0aadbbb865d91793648a
saravanan-22 <saravanan.skillsafari@gmail.com>
2023-08-14 21:43:59 -0700

    JS_Variables_workout-code

---
+++ hosting.js
@@ -0,0 +1,19 @@
+// function name(params) {
+//     console.log(name);
+// var name = "John";
+// // console.log(name);
+
+// }
+// name();
+
+//         function myFunction() {
+//   console.log(x); // undefined
+//   var x = "Hello, world!";
+//   console.log(x); // "Hello, world!"
+// }
+
+let a = 10;
+--a;
+console.log(a);
+
+myFunction();
---
+++ var.js
@@ -0,0 +1,12 @@
+let name = prompt(`Enter the name`);
+
+function functionName(params) {
+  if (name == fruits) {
+    let fruits = "mango, apple, greps";
+    // console.log(fruits);
+  } else {
+    // console.log("enter the valid name");
+    console.log("apple");
+  }
+}
+functionName();
